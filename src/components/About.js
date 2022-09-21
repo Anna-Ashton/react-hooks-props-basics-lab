fe2062b2e31325a16d12a1efa912dbee1de13b82
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-09-21 20:07:04 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+
+function ifBioEmpty(bio) {
+  if(bio) {
+    return <p>{bio}</p>
+  }
 }
+function About(props) {
+
+  // if (!props.bio) {
+  //   return null;
+  // } else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        {ifBioEmpty(props.bio)}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links linkedin={props.linkedin} github={props.github}/>
+      </div>
+    );
+  }
+  
+
+
+
+
 
 export default About;
